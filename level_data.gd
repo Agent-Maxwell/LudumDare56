ac65169888046cbe4a8543e1388b2c3d4a9e8588
Maxwell <avonbraunbehrens@gmail.com>
2024-10-05 15:24:35 -0600

    Update level_data.gd

--- level_data.gd
+++ level_data.gd
@@ -1,7 +1,7 @@
 extends Node
 
 #amount of cats for each level
-var catAmount = [1,2,5]
+var catAmount = [5,10,15]
 
 #obstacles to unhide for each level
 #format [array0(level 0)[obstaclename1, obstaclename2, etc], array1(level 1)[obstaclename1, obstaclename2, etc], [etc]]
