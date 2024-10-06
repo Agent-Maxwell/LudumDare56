6397046695d606edf74651030825206dc82a8dc2
Volohov7 <57515316+Volohov7@users.noreply.github.com>
2024-10-06 11:03:09 -0600

    cat bounce/grounded logic

--- cat.gd
+++ cat.gd
@@ -4,6 +4,10 @@ extends RigidBody3D
 var score = 0
 var multiplier = 1
 
+@onready var groundRay = $GroundRayCast
+var state
+var prevVelocity # to detect bounce collision speed, need to compare current vel with prev vel
+
 #track whether we have been kicked or grabbed (or both)
 var beenKicked = false
 var beenGrabbed = false
@@ -12,7 +16,8 @@ var beenGrabbed = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	set_state("airborne")
+	prevVelocity = linear_velocity
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -23,6 +28,24 @@ func _process(delta):
 		beenKicked = false
 		beenGrabbed = false
 		
+
+func _physics_process(delta):
+	# Bounce logic
+	var deltaV = (linear_velocity - prevVelocity).length() # deltaV = change in velocity since last physics step
+	if state == "airborne": #if airborne:
+		if deltaV > 1: #if there was a collision,
+			if deltaV > 20: #if it was fast, play bounce sound;
+				$Bounce.play()
+			else: #if it was slow, play soft impact sound,
+				$SoftImpact.play()
+				if groundRay.is_colliding(): #and if it was slow AND on the ground, land
+					set_state("grounded")
+	prevVelocity = linear_velocity
+	
+	if state == "grounded": # destroy upwards vertical velocity while grounded
+		linear_velocity.y = min(linear_velocity.y, 0)
+	
+	print(state)
 
 # player be like "what am i holding now"
 func type():
@@ -40,7 +63,19 @@ func resetScore():
 func launch_meow():
 	$ThrowMeow.play()
 
-#bounce sound plays if it bounces hard enough
-func _on_body_entered(body):
-	if (linear_velocity.length() > 10):
-		$Bounce.play()
+#func _on_body_entered(body):
+	#pass
+
+# manager for setting airborne/grounded state
+func set_state(newState):
+	state = newState
+	if state == "grounded":
+		linear_damp = 5
+		physics_material_override.bounce = 0
+		#if linear_velocity.length() < 2:
+			#linear_velocity *= Vector3(0, 1, 0) #destroy all horizontal velocity
+	elif state == "airborne":
+		linear_damp = 0
+		physics_material_override.bounce = 0.7
+	else:
+		print("error wtf is my state rn (-the cat)")
