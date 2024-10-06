244d6fe17e61287641e385815c8cc92d6a129172
Maxwell <avonbraunbehrens@gmail.com>
2024-10-06 14:58:06 -0600

    grill

--- level_data.gd
+++ level_data.gd
@@ -5,8 +5,8 @@ var catAmount = [1,5,10]
 
 #obstacles to unhide for each level
 #format [array0(level 0)[obstaclename1, obstaclename2, etc], array1(level 1)[obstaclename1, obstaclename2, etc], [etc]]
-var unhideObstacles = [["Tree",],["Trampoline"],["TallFence"]]
+var unhideObstacles = [["Grill",],["Trampoline"],["TallFence"]]
 
 #obstacles to have active for each level (all obstacles are deactivated between levels)
 #format [array0(level 0)[obstaclename1, obstaclename2, etc], array1(level 1)[obstaclename1, obstaclename2, etc], [etc]]
-var activateObstacles = [["Tree"],["Tree", "Trampoline"],["TallFence", "Tree"]]
+var activateObstacles = [["Grill"],["Grill", "Trampoline"],["TallFence", "Grill"]]
--- tree.gd
+++ tree.gd
@@ -2,4 +2,4 @@ extends MeshInstance3D
 
 func _on_area_3d_body_entered(body):
 	body.score += 50
-	$"../../Player".scoreMessage("Tree!")
+	$"../../Player".scoreMessage("Grill! (+50)")
