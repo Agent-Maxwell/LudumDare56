4556fe5edd560fb8cea3c2605dc636168c141ec7
Paul Nuffer <78831963+PaulNuffer@users.noreply.github.com>
2024-10-06 18:03:34 -0600

    cats being scored bouncing fixed, fence getting you stuck fixed

--- cat_checker_a.gd
+++ cat_checker_a.gd
@@ -18,6 +18,7 @@ func _on_body_entered(body):
 		#score it
 		$"..".score_points(body.score, body.multiplier, "Cat Scored! Total " + str(body.score))
 		#then stop it
+		body.set_state("grounded")
 		#body.linear_velocity = Vector3(0,0,0)
 
 #if a cat exits, decrement the cat count by 1
